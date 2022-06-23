3e09b8fffd510586790762ec2451027700077f22
BeainaT <tania.df@hotmail.it>
2022-06-23 18:43:53 +0200

    refactoring css + header completed

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ require 'php-data/data.php';
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP Google FAQs</title>
+        <!--fontawesome-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <!--google fonts-->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -22,15 +24,19 @@ require 'php-data/data.php';
                 <div class="header_top">
                     <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="">
                     <span>Privacy e termini</span>
+                    <i class="fa-solid fa-grip"></i>
+                    <a href="#">T</a>
                 </div>
-                <nav>
-                    <ul>
-                        <?php 
-                        foreach($links_nav as $link):?>
-                        <li><a href="#"><?= $link ?></a></li>
-                        <?php endforeach; ?>
-                    </ul>
-                </nav>
+                <div class="header_nav">
+                    <nav>
+                        <ul>
+                            <?php 
+                            foreach($links_nav as $link):?>
+                            <li><a href="#"><?= $link ?></a></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </header>
         <main>
