5c9dd8e9f7c8d0e085d32a42fbe317dbdf65b157
BeainaT <tania.df@hotmail.it>
2022-06-23 15:52:47 +0200

    added php code in html+fixed tag in data.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 //import data from another php file
 require 'php-data/data.php';
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Google FAQs</title>
+    </head>
+    <body>
+        <main>
+            <?php
+            foreach($FAQs as $FAQ):?>
+                <h2><?= $FAQ['question'] ?></h2>
+                <p><?= $FAQ['answer'] ?></p>
+            <?php endforeach; ?>
+        </main>
+    </body>
+</html>
